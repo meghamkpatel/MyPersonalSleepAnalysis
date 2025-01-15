94721e58ee8b4b6d988b51a914509d2afb386f43
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2025-01-15 17:22:05 -0500

    tesseract file location testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ gcs_credentials = {
 openai_client = openai.OpenAI(api_key=openai_api_key)
 
 # Set the Tesseract path
-if os.getenv("TESSERACT_PATH"):  # Use custom environment variable if set
-    pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_PATH")
+if st.secrets("TESSERACT_PATH"):  # Use custom environment variable if set
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 else:  # Default path (local system)
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
